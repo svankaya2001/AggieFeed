44e59569e6552eae1a2b2a6864875dbec7e65ab2
Shreyas Vankayalapati <59900350+svankaya2001@users.noreply.github.com>
2021-02-01 12:29:43 -0800

    updated query param

--- app/src/main/java/com/example/aggiefeed/PostClient.java
+++ app/src/main/java/com/example/aggiefeed/PostClient.java
@@ -4,7 +4,7 @@ import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 public class PostClient {
-    public static final String url = "https://aggiefeed.ucdavis.edu/api/v1/activity/public?l=25";
+    public static final String url = "https://aggiefeed.ucdavis.edu/api/v1/activity/public?s=0&l=25";
     private AsyncHttpClient asyncHttpClient;
 
     public PostClient(){
